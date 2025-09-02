4c364538218507d0a183b183280049408ad9383d
DavidPujol <dpujol@edu.tecnocampus.cat>
2025-09-02 19:21:33 +0200

    git commit -m "Initial commit: Setup E-Learning Platform APi - Spring Boot REST API project setup - PostgreSQL database configuration - JWT authentication dependencies - OpenAPI/Swagger documentation - Basic project structure and configuration

---
+++ src/main/java/com/portfolio/elearningplatformapi/ElearningPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.elearningplatformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningPlatformApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElearningPlatformApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/elearningplatformapi/ElearningPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.elearningplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningPlatformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
